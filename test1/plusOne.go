be3664a6f5487c89732e0f8a96a7ac24632fa732
dreamyoung2852 <294314959@qq.com>
2025-09-04 15:40:24 +0800

    plusOne

---
+++ test1/plusOne.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{
+		7, 6, 5,
+	}
+
+	fmt.Println(plusOne(nums))
+}
+
+//1.排序
+//2.进位拆分
+func plusOne(digits []int) []int {
+	var result []int
+
+	return result
+}
