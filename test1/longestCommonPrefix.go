6f4c07334100b02875782eab27f486acf19ad65e
dreamyoung2852 <294314959@qq.com>
2025-09-05 09:24:36 +0800

    commit

---
+++ test1/longestCommonPrefix.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	strs := []string{
+		"flower", "flow", "flight",
+	}
+
+	fmt.Println(longestCommonPrefix(strs))
+}
+
+func longestCommonPrefix(strs []string) string {
+
+	str1 := strs[0]
+
+	for s := range strs {
+
+		strings.HasPrefix(str, prefix1)
+	}
+
+}
